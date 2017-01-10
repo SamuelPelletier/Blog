7597ba2df549296e9597531470e9448e7d8279bc
Sam <samuel.pelletier@hotmail.fr>
2017-01-10 17:01:58 +0100

    Ajout de la vue View

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -15,7 +15,15 @@ class DefaultController extends Controller
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            'base_dir' => 'Hello',
         ]);
+    }
+    
+    /**
+     * @Route("/{id}", name="show_article")
+     */
+    public function showAction(Request $request, $id)
+    {	
+    	return $this->render('default/show.html.twig',array('id'=>$id));
     }
 }
